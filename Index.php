112d13c5b4e4dfabce193fb35bdc0df8e9f776cf
DavidSanchez21 <cdulo14@hotmail.com>
2014-12-08 22:39:37 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="../Facturas/config/estilos.css">
+<title>Facturas</title>
+</head>
+<body>
+
+	<form id="loginDiv" method="post" action="../Facturas/controlador/login.php"> 
+		<table>
+			<tr>
+				<td class="labels"><label id="usuario">Username </label></td>
+				<td><input type="text" name="user" id="user"></td>
+			</tr>
+			<tr>
+				<td class="labels"><label id="usuario">Password</label></td>
+				<td><input type="password" name="pass" id="pass"></td>
+			</tr>
+		</table>
+		</br>
+	<input type="submit" value="Enviar" id="BotonPass">
+</form>	
+
+</body>
+</html>
